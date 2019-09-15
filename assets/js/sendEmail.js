3a8eba85e2a9578d28c37d22f07a6ddc749326d8
David <haydal.tmps@gmail.com>
2019-09-15 15:24:35 +0100

    fixed bug for emailjs not working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,13 +1,19 @@
-function sendMail(contactForm) {
+function sendMail(contact) {
     emailjs.send("gmail", "debra_hayden_counselling_webpage", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "message": contact.message.value,
+        "from_name": contact.name.value,
+        "from_email": contact.emailaddress.value,
+        "from_phone_number": contact.phone.value,
     })
-    .then(
-        function(response) {
-        console.log('SUCCESS!', response);
-     }, 
-     function(error) {
-        console.log('FAILED...', error);
-     });
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                setTimeout("location.href = '#contact_after_submit';", 250); // the user will know that their message was sent successfully and is given feedback.
+
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page, this was originally in the tutorial notes.
 }
